334175046d8a52e8628119eb1c6f8f06a300b190
jinal--shah <jnshah@gmail.com>
2016-07-05 19:13:45 +0000

    removed interactive list-timers command

--- coreos/aws/scripts/00090-enable_ecr_login.sh
+++ coreos/aws/scripts/00090-enable_ecr_login.sh
@@ -5,13 +5,15 @@
 #
 # enable systemd timer to freshen creds for ecr
 #
+# This only checks that the timer is installed
+# - it doesn't check that the service is working!
+#    
 TIMER=ecr_login
-systemctl daemon-reload
 systemctl enable $TIMER.timer
+systemctl daemon-reload
 if ! systemctl start $TIMER.timer
 then
     echo "$0 ERROR: could not start up $TIMER.timer." >&2
     exit 1
 fi
 
-systemctl list-timers $TIMER.timer
